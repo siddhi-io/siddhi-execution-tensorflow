dd5983a758e702003bfd5025e27233cc6d395e2a
Chiran <pcnfernando@gmail.com>
2019-09-02 20:17:59 +0530

    Bump Tensorflow version

--- component/src/main/java/io/siddhi/extension/execution/tensorflow/TensorFlowExtension.java
+++ component/src/main/java/io/siddhi/extension/execution/tensorflow/TensorFlowExtension.java
@@ -226,14 +226,14 @@ public class TensorFlowExtension extends StreamProcessor<State> {
             }
 
             //Running the session and getting the output tensors
-            List<Tensor> outputTensors = tensorFlowRunner.run();
+            List<?> outputTensors = tensorFlowRunner.run();
 
             //Closing the input tensors to release resources (Tensors must be explicitly closed)
             for (Tensor t : inputTensors) {
                 t.close();
             }
 
-            complexEventPopulater.populateComplexEvent(streamEvent, getOutputObjectArray(outputTensors));
+            complexEventPopulater.populateComplexEvent(streamEvent, getOutputObjectArray((List<Tensor>) outputTensors));
         }
         nextProcessor.process(complexEventChunk);
     }
